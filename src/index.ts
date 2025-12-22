0ac7348be0e2eb6263a3653d8b104a8d6f2baba7
kangbeomsu <kbs21c1987@gmail.com>
2025-12-22 21:01:04 +0900

    [DEV-18] feat: prettier 설치 및 lint 설정

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import { Hono } from 'hono'
-import {envConfig} from "./config/env";
+import { Hono } from "hono";
+import { envConfig } from "./config/env";
+import { logger } from "hono/logger";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(
+  logger((message: string, ...rest: string[]) => {
+    console.log(message, ...rest);
+  }),
+);
 
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
 export default {
   port: envConfig.PORT || 8000,
